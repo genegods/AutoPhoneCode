bf47b6aceb2dce862103b6911a4f9e2affb5dc9a
Good Do <56465252+genegods@users.noreply.github.com>
2022-02-16 22:05:56 +0800

    Add files via upload

---
+++ autoPhonCodeApp/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import AutoPhoneApp from './components/AutoPhoneApp'
+
+
+
+
+
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <AutoPhoneApp/>
+                        
+    </React.Fragment>   
+  
+  );
+}
+
+export default App;
